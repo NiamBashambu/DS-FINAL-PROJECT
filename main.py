9cc88cc2329575bc04934d048f91581179789d3a
niamb2023 <niamb2023@headroyce.org>
2024-03-14 15:23:36 -0400

    changing the way the files are read

--- main.py
+++ main.py
@@ -4,27 +4,54 @@ import seaborn as sns
 import statistics
 import sys
 import os
+import pandas as pd
+import glob
 
 from utils import get_filename, read_csv, lst_to_dct, normalize, moving_avg
 
 DIR = "/Users/niambashambu/Desktop/DS FINAL PROJECT/data"
 FILENAME = "AAPL.csv"
+YEAR_HEADER = "Date"
 
-
-
+def clean_numeric(s):
+    ''' given a string with extra characters $ or , or %, remove them
+        and return the value as a float
+    '''
+    s = s.replace("$", "")
+    s = s.replace("%", "")
+    s = s.replace(",", "")
+    s= s.replace(":", "")
+    return float(s)
+def clean_data(dct):
+    ''' given a dictionary that includes currency and
+        numbers in the form x,xxx, clean them up and convert
+        to int/float
+    '''
+    for key, value in dct.items():
+        for i in range(len(value)):
+            if not value[i].replace(" ", "").isalpha():
+                value[i] = clean_numeric(value[i])
 
 def main():
-    data = read_csv(DIR + '/'+FILENAME)
-    dct = lst_to_dct(data)
+    
 
-    filenames = get_filename(DIR)
-    print(filenames)
+# Get a list of all CSV files in a directory
+    csv_files = glob.glob('data/*.csv')
+    
 
-    names = []
-    for filename in filenames:
-        lst = read_csv(filename)
-        dct = lst_to_dct(lst)
+# Create an empty dataframe to store the combined data
+    combined_df = pd.DataFrame()
 
+# Loop through each CSV file and append its contents to the combined dataframe
+    for csv_file in csv_files:
+        df = pd.read_csv(csv_file)
+       
+        df['filename'] = os.path.basename(csv_file)
+        combined_df = pd.concat([combined_df, df])
+
+# Print the combined dataframe
+    print(combined_df)
+    
 
 
 
