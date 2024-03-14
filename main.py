92a6cab5bc46e72381e49993a02747194881075a
niamb2023 <niamb2023@headroyce.org>
2024-03-14 15:06:50 -0400

    nvm

--- main.py
+++ main.py
@@ -12,18 +12,7 @@ FILENAME = "AAPL.csv"
 
 
 
-def get_filename(dirname, ext = ".csv"):
-    '''give a directory name (string), 
-    return the full path and name 
-    for every non directory file in the directory (list of strings)
-    '''
-    filenames = []
-    files = os.listdir(dirname)
-    for file in files:
-        full_path = os.path.join(dirname, file)  # Corrected to use the full path
-        if not os.path.isdir(full_path) and file.endswith(ext):  # Corrected check
-            filenames.append(full_path) 
-    return filenames
+
 def main():
     data = read_csv(DIR + '/'+FILENAME)
     dct = lst_to_dct(data)
